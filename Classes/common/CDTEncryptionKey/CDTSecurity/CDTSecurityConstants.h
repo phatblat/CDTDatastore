285d25cf4127c25a92d67b3ac842ef78a685abde
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-03-23 14:13:27 +0000

    First import with code to generate and safely store a DPK.
    The code is based on OpenSSL, this library is deprecated in MacOS and it is not included in iOS,
    therefore the code can only be compiled for MacOS. But before that, edit the Tests project and
    add the library:
    - Open Tests project
    - Select target 'Tests OSX'
    - Select 'Build Phases' and add library 'libcrypto.lib'

---
+++ Classes/common/CDTEncryptionKey/CDTSecurity/CDTSecurityConstants.h
@@ -0,0 +1,54 @@
+//
+//  CDTSecurityConstants.h
+//
+//
+//  Created by Enrique de la Torre Fernandez on 20/03/2015.
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+//
+
+#import <Foundation/Foundation.h>
+
+#warning Is it duplicated?
+extern int const CDTkChosenCipherKeySize;
+
+#warning Is it duplicated?
+extern int const CDTkChosenCipherIVSize;
+
+extern NSString *const CDTDATASTORE_SECURITY_BASE64_REGEX;
+
+extern NSString *const CDTDATASTORE_SECURITY_DEFAULT_ACCOUNT;
+
+extern int const CDTDATASTORE_SECURITY_DEFAULT_DPK_SIZE;
+extern int const CDTDATASTORE_SECURITY_DEFAULT_IV_SIZE;
+extern int const CDTDATASTORE_SECURITY_DEFAULT_PBKDF2_ITERATIONS;
+
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_LABEL;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_LABEL_KEYGEN;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_LABEL_ENCRYPT;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_LABEL_DECRYPT;
+
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_TEXT;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_KEY;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_IV;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_INVALID_ITERATIONS;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_PASSWORD;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_SALT;
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_EMPTY_CIPHER;
+
+extern NSString *const CDTDATASTORE_SECURITY_ERROR_MSG_INVALID_IV_LENGTH;
+
+extern NSString *const CDTDATASTORE_SECURITY_KEY_DPK;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_SALT;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_IV;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_ITERATIONS;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_VERSION;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_VERSION_NUMBER;
+extern NSString *const CDTDATASTORE_SECURITY_KEY_DOCUMENT_ID;
---
+++ Classes/common/CDTEncryptionKey/CDTSecurity/CDTSecurityManager.m
@@ -0,0 +1,257 @@
+//
+//  CDTSecurityManager.m
+//
+//
+//  Created by Enrique de la Torre Fernandez on 20/03/2015.
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+//
+
+#import "CDTSecurityManager.h"
+
+#import "CDTSecurityUtils.h"
+#import "CDTSecurityConstants.h"
+#import "NSObject+CDTSecurityJSON.h"
+#import "NSString+CDTSecurityJSON.h"
+
+#import "CDTLogging.h"
+
+@interface CDTSecurityManager ()
+
+@end
+
+@implementation CDTSecurityManager
+
+#pragma mark - Public methods
+- (NSString *)getDPK:(NSString *)password
+{
+    NSDictionary *storedDict = [self getDpKDocFromKeyChain];
+
+    if (storedDict == nil) {
+        return nil;
+    }
+
+    NSString *dpk = [storedDict objectForKey:CDTDATASTORE_SECURITY_KEY_DPK];
+    NSString *salt = [storedDict objectForKey:CDTDATASTORE_SECURITY_KEY_SALT];
+    NSString *pwKey = [self passwordToKey:password withSalt:salt];
+    NSString *iv = [storedDict objectForKey:CDTDATASTORE_SECURITY_KEY_IV];
+    NSString *decryptedKey = [CDTSecurityUtils decryptWithKey:pwKey
+                                               withCipherText:dpk
+                                                       withIV:iv
+                                  decodeBase64AfterDecryption:NO
+                                          checkBase64Encoding:YES];
+
+    return decryptedKey;
+}
+
+- (BOOL)generateAndStoreDpkUsingPassword:(NSString *)password withSalt:(NSString *)salt
+{
+    NSString *hexEncodedDpk =
+        [CDTSecurityUtils generateRandomStringWithBytes:CDTDATASTORE_SECURITY_DEFAULT_DPK_SIZE];
+
+    BOOL worked = [self storeDPK:hexEncodedDpk usingPassword:password withSalt:salt];
+
+    return worked;
+}
+
+- (BOOL)isKeyChainFullyPopulated
+{
+    if ([self checkDpkDocumentIsInKeychain]) {
+        return YES;
+    }
+
+    return NO;
+}
+
+- (BOOL)clearKeyChain
+{
+    BOOL worked = NO;
+
+    NSMutableDictionary* dict = [self getDpkDocumentLookupDict];
+    [dict removeObjectForKey:(__bridge id)(kSecReturnData)];
+    [dict removeObjectForKey:(__bridge id)(kSecMatchLimit)];
+    [dict removeObjectForKey:(__bridge id)(kSecReturnAttributes)];
+    [dict removeObjectForKey:(__bridge id)(kSecAttrAccount)];
+
+    OSStatus err = SecItemDelete((__bridge CFDictionaryRef)dict);
+
+    if (err == noErr || err == errSecItemNotFound) {
+        worked = YES;
+    } else {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT,
+                   @"Error getting DPK doc from keychain, SecItemDelete returned: %d", err);
+    }
+
+    return worked;
+}
+
+#pragma mark - Private methods
+- (BOOL)storeDPK:(NSString *)dpk usingPassword:(NSString *)password withSalt:(NSString *)salt
+{
+    BOOL worked = NO;
+
+    NSString *pwKey = [self passwordToKey:password withSalt:salt];
+
+    NSString *hexEncodedIv =
+        [CDTSecurityUtils generateRandomStringWithBytes:CDTDATASTORE_SECURITY_DEFAULT_IV_SIZE];
+
+    NSString *encyptedDPK = [CDTSecurityUtils encryptWithKey:pwKey
+                                                    withText:dpk
+                                                      withIV:hexEncodedIv
+                                covertBase64BeforeEncryption:NO];
+
+    NSDictionary *jsonEntriesDict = @{
+        CDTDATASTORE_SECURITY_KEY_IV : hexEncodedIv,
+        CDTDATASTORE_SECURITY_KEY_SALT : salt,
+        CDTDATASTORE_SECURITY_KEY_DPK : encyptedDPK,
+        CDTDATASTORE_SECURITY_KEY_ITERATIONS :
+            [NSNumber numberWithInt:CDTDATASTORE_SECURITY_DEFAULT_PBKDF2_ITERATIONS],
+        CDTDATASTORE_SECURITY_KEY_VERSION : CDTDATASTORE_SECURITY_KEY_VERSION_NUMBER
+    };
+
+    NSString *jsonStr = [jsonEntriesDict CDTSecurityJSONRepresentation];
+    NSMutableDictionary *jsonDocStoreDict =
+        [self getGenericPwStoreDict:CDTDATASTORE_SECURITY_KEY_DOCUMENT_ID data:jsonStr];
+
+    OSStatus err = SecItemAdd((__bridge CFDictionaryRef)jsonDocStoreDict, nil);
+    if (err == noErr) {
+        worked = YES;
+    } else if (err == errSecDuplicateItem) {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT, @"Doc already exists in keychain");
+        worked = NO;
+    } else {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT,
+                   @"Unable to store Doc in keychain, SecItemAdd returned: %d", err);
+        worked = NO;
+    }
+
+    return worked;
+}
+
+- (NSDictionary *)getDpKDocFromKeyChain
+{
+    NSMutableDictionary *lookupDict = [self getDpkDocumentLookupDict];
+
+    NSData *theData = nil;
+
+    OSStatus err = SecItemCopyMatching((__bridge CFDictionaryRef)lookupDict, (void *)&theData);
+
+    if (err == noErr) {
+        NSString *jsonStr = [[NSString alloc] initWithBytes:[theData bytes]
+                                                     length:[theData length]
+                                                   encoding:NSUTF8StringEncoding];
+
+        id jsonDoc = [jsonStr CDTSecurityJSONValue];
+
+        if (jsonDoc != nil && [jsonDoc isKindOfClass:[NSDictionary class]]) {
+            // Ensure the num derivations saved, matches what we have
+            int iters = [[(NSDictionary *)jsonDoc
+                objectForKey:CDTDATASTORE_SECURITY_KEY_ITERATIONS] intValue];
+
+            if (iters != CDTDATASTORE_SECURITY_DEFAULT_PBKDF2_ITERATIONS) {
+                CDTLogWarn(CDTDATASTORE_LOG_CONTEXT,
+                           @"Number of iterations stored, does NOT match the constant value %u",
+                           CDTDATASTORE_SECURITY_DEFAULT_PBKDF2_ITERATIONS);
+                return nil;
+            }
+
+            return jsonDoc;
+        }
+    } else {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT,
+                   @"Error getting DPK doc from keychain, SecItemCopyMatching returned: %d", err);
+    }
+
+    return nil;
+}
+
+- (BOOL)checkDpkDocumentIsInKeychain
+{
+    NSData* dpkData = nil;
+
+    OSStatus err = SecItemCopyMatching((__bridge CFDictionaryRef)[self getDpkDocumentLookupDict],
+                                       (void*)&dpkData);
+
+    if (err == noErr) {
+        NSString* dpk = [[NSString alloc] initWithBytes:[dpkData bytes]
+                                                 length:[dpkData length]
+                                               encoding:NSUTF8StringEncoding];
+
+        if (dpk != nil && [dpk length] > 0) {
+            return YES;
+
+        } else {
+            // Found a match in keychain, but it was empty
+            return NO;
+        }
+
+    } else if (err == errSecItemNotFound) {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT, @"DPK doc not found in keychain");
+        
+        return NO;
+    } else {
+        CDTLogWarn(CDTDATASTORE_LOG_CONTEXT,
+                   @"Error getting DPK doc from keychain, SecItemCopyMatching returned: %d", err);
+        
+        return NO;
+    }
+}
+
+- (NSString *)passwordToKey:(NSString *)password withSalt:(NSString *)salt
+{
+    return
+        [CDTSecurityUtils generateKeyWithPassword:password
+                                          andSalt:salt
+                                    andIterations:CDTDATASTORE_SECURITY_DEFAULT_PBKDF2_ITERATIONS];
+}
+
+- (NSMutableDictionary *)getDpkDocumentLookupDict
+{
+    NSMutableDictionary *dpkQuery =
+        [self getGenericPwLookupDict:CDTDATASTORE_SECURITY_KEY_DOCUMENT_ID];
+    return dpkQuery;
+}
+
+- (NSMutableDictionary *)getGenericPwLookupDict:(NSString *)identifier
+{
+    NSMutableDictionary *genericPasswordQuery = [[NSMutableDictionary alloc] init];
+    [genericPasswordQuery setObject:(__bridge id)kSecClassGenericPassword
+                             forKey:(__bridge id)kSecClass];
+    [genericPasswordQuery setObject:CDTDATASTORE_SECURITY_DEFAULT_ACCOUNT
+                             forKey:(__bridge id<NSCopying>)(kSecAttrAccount)];
+    [genericPasswordQuery setObject:identifier forKey:(__bridge id<NSCopying>)(kSecAttrService)];
+
+    // Use the proper search constants, return only the attributes of the first match.
+    [genericPasswordQuery setObject:(__bridge id)kSecMatchLimitOne
+                             forKey:(__bridge id<NSCopying>)(kSecMatchLimit)];
+    [genericPasswordQuery setObject:(__bridge id)kCFBooleanFalse
+                             forKey:(__bridge id<NSCopying>)(kSecReturnAttributes)];
+    [genericPasswordQuery setObject:(__bridge id)kCFBooleanTrue
+                             forKey:(__bridge id<NSCopying>)(kSecReturnData)];
+    return genericPasswordQuery;
+}
+
+- (NSMutableDictionary *)getGenericPwStoreDict:(NSString *)identifier data:(NSString *)theData
+{
+    NSMutableDictionary *genericPasswordQuery = [[NSMutableDictionary alloc] init];
+    [genericPasswordQuery setObject:(__bridge id)kSecClassGenericPassword
+                             forKey:(__bridge id)kSecClass];
+    [genericPasswordQuery setObject:CDTDATASTORE_SECURITY_DEFAULT_ACCOUNT
+                             forKey:(__bridge id<NSCopying>)(kSecAttrAccount)];
+    [genericPasswordQuery setObject:identifier forKey:(__bridge id<NSCopying>)(kSecAttrService)];
+    [genericPasswordQuery setObject:[theData dataUsingEncoding:NSUTF8StringEncoding]
+                             forKey:(__bridge id<NSCopying>)(kSecValueData)];
+    [genericPasswordQuery setObject:(__bridge id)(kSecAttrAccessibleAlways)
+                             forKey:(__bridge id<NSCopying>)(kSecAttrAccessible)];
+
+    return genericPasswordQuery;
+}
+
+@end
