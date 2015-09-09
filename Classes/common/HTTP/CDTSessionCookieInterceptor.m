c07101c5094826ecee2b3dc6e9316599a066c6aa
Rhys Short <rhyshort@uk.ibm.com>
2015-09-09 17:21:27 +0100

    Rename CDTCookieInterceptor to CDTSessionCookieInterceptor

--- Classes/common/HTTP/CDTSessionCookieInterceptor.m
+++ Classes/common/HTTP/CDTSessionCookieInterceptor.m
@@ -1,15 +1,22 @@
 //
-//  CDTCookieInterceptor.m
+//  CDTSessionCookieInterceptor.m
 //
 //
 //  Created by Rhys Short on 08/09/2015.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
 //
 
-#import "CDTCookieInterceptor.h"
+#import "CDTSessionCookieInterceptor.h"
 #import "CDTLogging.h"
 
-@interface CDTCookieInterceptor ()
+@interface CDTSessionCookieInterceptor ()
 
 @property (nonnull, strong, nonatomic) NSString *cookieRequestBody;
 @property (nonatomic) BOOL shouldMakeCookieRequest;
@@ -18,7 +25,7 @@
 
 @end
 
-@implementation CDTCookieInterceptor
+@implementation CDTSessionCookieInterceptor
 
 - (instancetype)initWithUsername:(NSString *)username password:(NSString *)password
 {
